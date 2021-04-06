7dd76e4b61325de16f359dc5eda96c91bf2d97ae
Rob <robert.cook@bcu.ac.uk>
2021-04-06 12:34:42 +0100

    Change Template import.

--- nurs_data_reference/reference_to_markdown.py
+++ nurs_data_reference/reference_to_markdown.py
@@ -1,13 +1,11 @@
+from importlib.resources import read_text
 
-with open("nurs_data_reference/template.txt", "r") as f:
-    TEMPLATE = f.read()
+TEMPLATE = read_text("nurs_data_reference", "template.txt")
 
 def reference_to_markdown(**kwargs):
-    kwargs["Common_1"] = "|".join(kwargs.get("Common 1"))
-    kwargs["Common_2"] = "|".join(kwargs.get("Common 2"))
-    kwargs["Common_3"] = "|".join(kwargs.get("Common 3"))
+    kwargs["Common_1"] = "|".join(map(str, kwargs.get("Common 1")))
+    kwargs["Common_2"] = "|".join(map(str, kwargs.get("Common 2")))
+    kwargs["Common_3"] = "|".join(map(str, kwargs.get("Common 3")))
     return TEMPLATE.format(
         **kwargs
     )
-
-
