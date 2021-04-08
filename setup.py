6a4cb24d8c3caa2148406326acf6a9a7aec922c8
Rob <robert.cook@bcu.ac.uk>
2021-04-08 13:07:35 +0100

    Add word2ref to package list

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(
     name='nurs_data_reference',
     version='0.1.0',
-    packages=['nurs_data_reference'],
+    packages=['nurs_data_reference',
+              "nurs_data_reference.word2reference"],
     include_package_data=True
 )
