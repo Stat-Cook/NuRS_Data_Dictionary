a4cdfbb772846e76372649bb4738b6de0d5bbd61
Rob <robert.cook@bcu.ac.uk>
2021-04-16 15:59:51 +0100

    Fix bug with description_path not loading file.

--- nurs_data_reference/cli/nurs_reference_spider.py
+++ nurs_data_reference/cli/nurs_reference_spider.py
@@ -5,6 +5,8 @@ from ..api import find_all_columns
 def main(argv=None):
     args = spider_parser(argv)
 
-    grouped_frame = find_all_columns(args.Path, args.description_path)
+    grouped_frame = find_all_columns(args.Path)
+    if args.description_path:
+        grouped_frame.description_frame.from_word(args.description_path)
     grouped_frame.to_markdown(f"{args.markdown_path}.md")
     grouped_frame.description_frame.to_word(f"{args.new_description_path}.docx")
