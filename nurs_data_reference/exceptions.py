b60afc53dd985bf42f00aaf377e20388b9d231cc
Rob <robert.cook@bcu.ac.uk>
2021-04-16 10:52:05 +0100

    New exception for missing columns.

--- nurs_data_reference/exceptions.py
+++ nurs_data_reference/exceptions.py
@@ -5,3 +5,7 @@ Custom exceptions for project.
 
 class OverlapException(Exception):
     """Custom exception if values already exist on merge."""
+
+
+class MissingColumnsException(Exception):
+    """Custom exception if values already exist on merge."""
